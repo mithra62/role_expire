9660601b67162e608ee02b7c2cfce32b2117583b
Eric Lamb <eric@mithra62.com>
2024-01-24 11:12:12 -0700

    psr12

--- system/user/addons/role_expire/addon.setup.php
+++ system/user/addons/role_expire/addon.setup.php
@@ -2,17 +2,16 @@
 
 use Mithra62\RoleExpire\Services\RolesService;
 
-const ROLE_EXPIRE_VERSION = '1.0.0';
+const ROLE_EXPIRE_VERSION = '1.0.1';
 
 return [
-    'author'            => 'mithra62',
-    'author_url'        => '',
-    'name'              => 'Role Expire',
-    'description'       => 'Allows for Member Roles to expire after a set number of seconds from creation',
-    'version'           => ROLE_EXPIRE_VERSION,
-    'namespace'         => 'Mithra62\RoleExpire',
-    'settings_exist'    => true,
-    // Advanced settings
+    'author' => 'mithra62',
+    'author_url' => 'https://github.com/mithra62',
+    'name' => 'Role Expire',
+    'description' => 'Allows for Member Roles to expire after a set number of seconds from creation',
+    'version' => ROLE_EXPIRE_VERSION,
+    'namespace' => 'Mithra62\RoleExpire',
+    'settings_exist' => true,
     'models' => [
         'Settings' => 'Model\RoleExpire',
         'Member' => 'Model\Member',
@@ -21,5 +20,5 @@ return [
         'RolesService' => function ($addon) {
             return new RolesService;
         },
-    ]
+    ],
 ];
